bf60544932153075a96d6f775d75e8c83bc411cf
kushal-rajkarnikar <kushal.rajkarnikar@gmail.com>
2019-01-02 17:19:22 +0545

    Added menus

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Newslist from './components/newslist';
 const API = 'https://newsapi.org/v2/';
 const KEY = '&apiKey=f0c7756f333948cf8fe88ae2896cc1f3'; 
 
-let DEFAULT_QUERY = 'top-headlines?q=world';
+let DEFAULT_QUERY = 'top-headlines?category=sports';
+
+let CATEGORY_QUERY = 'top-headlines?category=';
 
 class App extends Component {
 
@@ -14,7 +16,8 @@ class App extends Component {
     super(props);
 
     this.state = {
-      news: []
+      news: [],
+      category: ''
     }
   }
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+    state = {
+        category: ''
+    }
+    setNewsCategory = (categoryClicked) => {
+        this.setState({category: categoryClicked});
+    }
+
     render() {
         return (            
             <nav className="navbar navbar-default yamm navbar-fixed-top" role="navigation" id="inheadlines-navbar">
@@ -17,44 +24,38 @@ class Header extends Component {
             </button>            
             </div>
             
-            
-            <div className="collapse navbar-collapse navbar-ex1-collapse" id="navbar">
-            
-            
-            <ul className="nav navbar-nav">
-            <li className="dropdown yamm-fw">
-            {/* <a href="#" className="dropdown-toggle" data-toggle="dropdown" id="categories">Categories</a> */}
-            <ul className="dropdown-menu">
-            <div className="container narrow-container">
-            <li><a href="#">all news</a></li>
-            <li><a href="#">top stories</a></li>
-            <li><a href="#">trending</a></li>
-            <li><a href="#">national</a></li>
-            <li><a href="#">captial</a></li>
-            
-            <li><a href="#">nepse</a></li>
-            <li><a href="#">politics</a></li>
-            <li><a href="#">business</a></li>
-            <li><a href="#">economy</a></li>
-            
-            <li><a href="#">sports</a></li>
-            <li><a href="#">technology</a></li>
-            <li><a href="#">startups</a></li>
-            <li><a href="#">energy</a></li>
-            <li><a href="#">science</a></li>
-            <li><a href="#">entertainment</a></li>
-            <li><a href="#">travel</a></li>
-            <li><a href="#">world</a></li>
-            <li><a href="#">miscellaneous</a></li>
-            </div>
+            <ul class="navigation">
+                <li>
+                <a href="#" onClick={() => { this.setNewsCategory("Politics"); }}>
+                    Politics
+                </a>
+            </li>                
+            <li>
+                <a href="#">
+                    Business
+                </a>
+            </li>   
+            <li>
+                <a href="#">
+                    Sports
+                </a>
+            </li>   
+            <li>
+                <a href="#">
+                    Entertainment
+                </a>
+            </li>   
+                        <li>
+                            <a href="#">
+                                Environment
+                </a>
+                        </li>   
             </ul>
-            </li>
-            </ul>
-
-            </div>
+            
+            
             </div>
             </nav>
             )
         }
     }
-export default Header;
+    export default Header;
